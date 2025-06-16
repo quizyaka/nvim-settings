33780e6712ce044c14bae11c2bb31bf1c7f5de3e
quizyaka <bodea057@gmail.com>
2025-06-16 17:13:59 +0300

    clear

--- init.lua
+++ init.lua
@@ -8,14 +8,3 @@ require('config.prettier')
 
 require('config.treesitter')
 require('config.transparent')
-
--- Статусная строка и табы
---[=[
-vim.cmd([[
-    hi StatusLine guibg=NONE ctermbg=NONE gui=NONE cterm=NONE
-    hi StatusLineNC guibg=NONE ctermbg=NONE
-  ]])
-    ]=]
--- Границы и разделители
-
--- Особые элементы (NvimTree, Telescope и др.)
